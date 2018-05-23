745af119095478e1ec222ffde32a9a7cf0f4a001
kdrag0n <dragon@khronodragon.com>
2018-05-22 19:29:17 -0700

    random: always use non-blocking pool
    The non-blocking pool (urandom) is significantly faster while also
    being more secure because it is a ChaCha20 CRNG now.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1849,12 +1849,6 @@ _random_read(int nonblock, char __user *buf, size_t nbytes)
 }
 
 static ssize_t
-random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
-{
-	return _random_read(file->f_flags & O_NONBLOCK, buf, nbytes);
-}
-
-static ssize_t
 urandom_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
 	unsigned long flags;
@@ -1997,7 +1991,7 @@ static int random_fasync(int fd, struct file *filp, int on)
 }
 
 const struct file_operations random_fops = {
-	.read  = random_read,
+	.read  = urandom_read,
 	.write = random_write,
 	.poll  = random_poll,
 	.unlocked_ioctl = random_ioctl,
