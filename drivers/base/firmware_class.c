015e113809bf01efda1371bac5b7cd57d5e39ffa
Nauval Rizky <enuma.alrizky@gmail.com>
2021-06-02 13:02:46 +0700

    base: firmware: Add Xiaomi system and vendor firmware path
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -382,6 +382,10 @@ static void fw_free_buf(struct firmware_buf *buf)
 static char fw_path_para[256];
 static const char * const fw_path[] = {
 	fw_path_para,
+#ifdef CONFIG_MACH_XIAOMI
+	"/system/vendor/firmware",
+	"/system/etc/firmware",
+#endif
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
