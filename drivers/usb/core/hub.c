8ad6844160bb9f820e35abdb598487b9be2d6fd6
Nauval Rizky <enuma.alrizky@gmail.com>
2021-06-07 06:02:51 +0700

    usb: core: hub: Properly opt-out xiaomi from LPM support
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4425,6 +4425,7 @@ static int hub_set_address(struct usb_device *udev, int devnum)
  * device says it supports the new USB 2.0 Link PM errata by setting the BESL
  * support bit in the BOS descriptor.
  */
+#ifndef CONFIG_MACH_XIAOMI // opted-out for xiaomi
 static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 {
 	struct usb_hub *hub = usb_hub_to_struct_hub(udev->parent);
@@ -4442,6 +4443,7 @@ static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 		usb_enable_usb2_hardware_lpm(udev);
 	}
 }
+#endif
 
 static int hub_enable_device(struct usb_device *udev)
 {
@@ -4795,7 +4797,9 @@ hub_port_init(struct usb_hub *hub, struct usb_device *udev, int port1,
 	if (hcd->driver->update_device)
 		hcd->driver->update_device(hcd, udev);
 	/* disable USB 2.0 Link PM to fix long time usb storage recognition issue */
-	// hub_set_initial_usb2_lpm_policy(udev);
+#ifndef CONFIG_MACH_XIAOMI
+	hub_set_initial_usb2_lpm_policy(udev);
+#endif
 fail:
 	if (retval) {
 		hub_port_disable(hub, port1, 0);
