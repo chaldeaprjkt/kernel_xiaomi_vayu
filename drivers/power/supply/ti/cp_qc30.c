33ddd4a2ef605230e53d1403a9087b7b61126b98
Jebaitedneko <Jebaitedneko@gmail.com>
2022-01-30 02:12:34 +0530

    power: supply: ti: add ln8000 pd, cp changes
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- drivers/power/supply/ti/cp_qc30.c
+++ drivers/power/supply/ti/cp_qc30.c
@@ -1017,6 +1017,11 @@ void cp_statemachine(unsigned int port)
 	}
 }
 
+static int cp_psy_is_lionsemi(void)
+{
+	return power_supply_get_by_name("ln8000");
+}
+
 static void cp_workfunc(struct work_struct *work)
 {
 	cp_get_usb_type();
@@ -1031,6 +1036,8 @@ static void cp_workfunc(struct work_struct *work)
 	/* check whether usb is present */
 	if (pm_state.usb_present == 0) {
 		cp_set_qc_bus_protections(HVDCP3_NONE);
+		if (cp_psy_is_lionsemi)
+			pm_state.state = CP_STATE_DISCONNECT;
 		return;
 	}
 
